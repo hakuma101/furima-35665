98dc36c59093239fec27017ce1ea4e942ff5ed84
hakuma101 <hakumai.1310@gmail.com>
2022-04-09 18:58:25 +0900

    Userモデルのテストコード
    Userモデルのテストコード用ファイルの作成、テストコードを記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname { 'furima太郎' }
     email { Faker::Internet.free_email }
-    password { '1a' + Faker::Internet.password(min_length: 6) }
+    password { 'a1' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     last_name { '山田' }
     first_name { '陸太郎' }
