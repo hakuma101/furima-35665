ec04e9a86dc2c1d43be38724183461a9a888f083
hakuma101 <hakumai.1310@gmail.com>
2022-04-10 13:54:03 +0900

    バリデーションの設定
    Itemモデルのバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,15 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  
+  validates :image,         presence: true
+
+  validates :product,       presence: true
+  validates :discription,   presence: true
+  validates :category_id,   numericality: { other_than: 1 }
+  validates :condition_id,  numericality: { other_than: 1 }
+  validates :cost_id,       numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :sending_id,    numericality: { other_than: 1 }
+  validates :price,         presence: true, format: { with: /\A[0-9]+\z/ }, numericality: { in: 300..9_999_999 }
+
 end
