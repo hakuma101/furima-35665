cc2e11befaf49162207e15eaecde2bf310b3d899
hakuma101 <hakumai.1310@gmail.com>
2022-04-10 16:18:58 +0900

    createアクション
    itemコントローラーにcreateアクションを設定
    入力されたデータを保存するよう記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,20 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:product, :description, :category_id, :condition_id, :cost_id, :prefecture_id,
+                                 :sending_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
