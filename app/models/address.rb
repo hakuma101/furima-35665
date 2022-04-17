2658bc1aa54523b100403481aaeb261d142ade20
hakuma101 <hakumai.1310@gmail.com>
2022-04-17 18:31:21 +0900

    バリデーションの移動
    order_addressモデルにorderモデルとaddressモデルにあったバリデーションを移動

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,9 +2,4 @@ class Address < ApplicationRecord
 
   belongs_to :order
 
-  validates :postal_code,    presence: true
-  validates :prefecture_id,  numericality: { other_than: 1, message: "can't be blank" }
-  validates :manicipality,   presence: true
-  validates :house_number,   presence: true
-  validates :phone_number,   presence: true
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,4 +1,10 @@
 class DonationAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :manicipality, :house_number, :building_name
+
+  validates :postal_code,    presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :prefecture_id,  numericality: { other_than: 1, message: "can't be blank" }
+  validates :manicipality,   presence: true
+  validates :house_number,   presence: true
+  validates :phone_number,   presence: true, format: {with: /\A[0-9]{,11}\z/, message: ""}
 end
