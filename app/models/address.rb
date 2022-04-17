778720670d86e06abd760d3eb540ae3fc7b9b3e5
hakuma101 <hakumai.1310@gmail.com>
2022-04-17 14:25:41 +0900

    addressモデルのバリデーション
    addressモデルのバリデーションを設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,7 @@
 class Address < ApplicationRecord
+  validates :postal_code,    presence: true
+  validates :prefecture_id,  numericality: { other_than: 1, message: "can't be blank" }
+  validates :manicipality,   presence: true
+  validates :house_number,   presence: true
+  validates :phone_number,   presence: true
 end
