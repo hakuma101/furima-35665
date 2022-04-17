3e62b9c5b665f334a2bcf15e70c8cf0df7768d8d
hakuma101 <hakumai.1310@gmail.com>
2022-04-17 19:43:52 +0900

    インスタンスの生成
    コントローラーでFormオブジェクトのインスタンスを生成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,9 +2,23 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def donation_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :manicipality, :house_number, :building_name).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
