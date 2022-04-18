6b8c64c817c4991eb1c0e5819180d15824c4876e
hakuma101 <hakumai.1310@gmail.com>
2022-04-18 19:04:01 +0900

    リファクタリング
    新たにメソッドを定義し、その中に決済処理を移動

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,12 +9,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-      Payjp.api_key = "sk_test_***********"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
-      Payjp::Charge.create(
-        amount: @item.price,           # 商品の値段
-        card: order_params[:token],    # カードトークン
-        currency: 'jpy'                # 通貨の種類（日本円）
-      )
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -26,6 +21,15 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :manicipality, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_***********"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price,           # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                # 通貨の種類（日本円）
+    )
   end
 
 end
