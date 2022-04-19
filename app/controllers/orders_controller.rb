cbed1ed2f41fcc6a91056ae05f11bb87c3828dc6
hakuma101 <hakumai.1310@gmail.com>
2022-04-19 12:46:27 +0900

    エラー表示確認
    エラー表示動作確認時のエラー箇所を修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
-  before_action :move_to_index, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
@@ -38,7 +38,7 @@ class OrdersController < ApplicationController
   end
 
   def set_item
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
   end
 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,14 +2,14 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :manicipality, :house_number, :building_name, :phone_number, :user_id, :item_id, :token
 
-  validates :postal_code,    presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :token,          presence: true
+  validates :postal_code,    presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
   validates :prefecture_id,  numericality: { other_than: 1, message: "can't be blank" }
   validates :manicipality,   presence: true
   validates :house_number,   presence: true
-  validates :phone_number,   presence: true, format: {with: /\A[0-9]{,11}\z/, message: ""}
+  validates :phone_number,   presence: true, format: {with: /\A\d{10,11}\z/, message: "is invalid. Input only number"}, length: { in: 10..11, message: "is too short"}
   validates :user_id,        presence: true
   validates :item_id,        presence: true
-  validates :token,          presence: true
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
